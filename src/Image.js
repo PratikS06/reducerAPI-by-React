53180dfc5c104382d32abb659c630511237fd7a9
unknown <pratiksawant3634@gmail.com>
2022-04-05 17:55:41 +0530

    Initial commit

---
+++ src/Image.js
@@ -0,0 +1,14 @@
+import React from "react";
+import img1 from "./logo.png"
+
+const Image = ()=>{
+    return(
+        <div>
+        <img src={img1} alt="" className="image"/>
+                
+        </div>
+    )
+}
+
+
+export default Image 
